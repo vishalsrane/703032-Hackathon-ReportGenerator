823d975a0d8d4ae557dd7c0a1171ca56db1905dd
vishalsrane <vishalsrane@gmail.com>
2019-03-10 14:08:06 +0530

    changed sequence generation to identity

--- src/main/java/com/ctsreporting/reportGenerator/model/EventDetails.java
+++ src/main/java/com/ctsreporting/reportGenerator/model/EventDetails.java
@@ -19,7 +19,7 @@ import lombok.Data;
 public class EventDetails {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	public Long getId() {
